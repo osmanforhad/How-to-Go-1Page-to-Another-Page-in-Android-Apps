4207724f68e73e69454c2fb566ddda7382442b08
osmanforhad <osmanforhad93@gmail.com>
2018-12-31 22:21:24 +0600

    How to go one page to another in android apps

---
+++ How-to-Go-1Page-to-Another-Page-in-Android-Apps/FriendsInfo/app/src/main/java/com/osmanforhad/friendsinfo/DetailsOfFriends.java
@@ -0,0 +1,13 @@
+package com.osmanforhad.friendsinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsOfFriends extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_of_friends);
+    }
+}
